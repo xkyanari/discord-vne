952cb35eb9361163b91f41e0418e40fd9b8d0fa5
Kyanari <kyanari@megura.xyz>
2023-11-19 12:58:09 -0800

    Updated with new chapters!

--- functions/vn.js
+++ functions/vn.js
@@ -7,64 +7,65 @@ const chaptersDirectory = path.join(__dirname, '../chapters');
 const charactersPath = path.join(__dirname, '../assets/characters.json');
 const settingsPath = path.join(__dirname, '../assets/settings.json');
 
-async function startNovel(interaction) {
-    const chapters = fs.readdirSync(chaptersDirectory);
+async function startNovel(interaction, chapter) {
+    // const chapters = fs.readdirSync(chaptersDirectory);
     const variablesContent = fs.readFileSync(charactersPath, "utf-8");
     const variablesData = JSON.parse(variablesContent);
     const settingsContent = fs.readFileSync(settingsPath, "utf-8");
     const settingsData = JSON.parse(settingsContent);
 
-    for (const chapter of chapters) {
-        const chapterPath = path.join(chaptersDirectory, chapter);
-        const chapterContent = fs.readFileSync(chapterPath, "utf-8");
-        const chapterData = JSON.parse(chapterContent);
+    const chapterFilename = `${chapter}.json`;
+    const chapterPath = path.join(chaptersDirectory, chapterFilename);
+    const chapterContent = fs.readFileSync(chapterPath, "utf-8");
+    const chapterData = JSON.parse(chapterContent);
 
-        if (chapterData.dialogues && chapterData.dialogues.length > 0) {
-            const embeds = [];
-            for (const dialogue of chapterData.dialogues) {
-                const characterVariables = variablesData[dialogue.character];
-                const settingVariables = settingsData[dialogue.setting];
+    if (chapterData.dialogues && chapterData.dialogues.length > 0) {
+        const embeds = [];
+        for (const dialogue of chapterData.dialogues) {
+            const characterVariables = variablesData[dialogue.character];
+            const settingVariables = settingsData[dialogue.setting];
 
-                if (!characterVariables) {
-                    console.log(`No variables found for character: ${dialogue.character}`);
+            if (!characterVariables) {
+                console.log(`No variables found for character: ${dialogue.character}`);
+                continue;
+            }
+
+            let currentEmbed = new EmbedBuilder();
+            if (dialogue.character === 'Narrator') {
+                currentEmbed.setColor(characterVariables.defaultColor);
+            }
+            else {
+                const expressionKey = dialogue.expression || characterVariables.defaultExpression;
+                const expressionVariables = characterVariables.expressions[expressionKey];
+
+                if (!expressionVariables) {
+                    console.log(`No variables found for expression: ${expressionKey}`);
                     continue;
                 }
 
-                let currentEmbed = new EmbedBuilder();
-                if (dialogue.character === 'Narrator') {
-                    currentEmbed.setColor(characterVariables.defaultColor);
-                }
-                else {
-                    const expressionKey = dialogue.expression || characterVariables.defaultExpression;
-                    const expressionVariables = characterVariables.expressions[expressionKey];
-    
-                    if (!expressionVariables) {
-                        console.log(`No variables found for expression: ${expressionKey}`);
-                        continue;
-                    }
-
-                    currentEmbed.setTitle(dialogue.character)
-                    currentEmbed.setColor(characterVariables.defaultColor)
-                    currentEmbed.setThumbnail(expressionVariables.image);
-                }
-
-                if (dialogue.dialogue) {
-                    currentEmbed.setDescription(dialogue.dialogue);
-                }
-
-                if (dialogue.thoughts) {
-                    currentEmbed.setDescription(`_${dialogue.thoughts}_`);
-                }
-
-                if (dialogue.setting) {
-                    currentEmbed.setImage(settingVariables.image);
-                }
-
-                embeds.push(currentEmbed);
+                currentEmbed.setTitle(dialogue.character)
+                currentEmbed.setColor(characterVariables.defaultColor)
+                currentEmbed.setThumbnail(expressionVariables.image);
             }
-            await buttonPages(interaction, embeds);
+
+            if (dialogue.dialogue) {
+                currentEmbed.setDescription(dialogue.dialogue);
+            }
+
+            if (dialogue.thoughts) {
+                currentEmbed.setDescription(`_${dialogue.thoughts}_`);
+            }
+
+            if (dialogue.setting) {
+                currentEmbed.setImage(settingVariables.image);
+            }
+
+            embeds.push(currentEmbed);
         }
+        await buttonPages(interaction, embeds);
     }
+    // for (const chapter of chapters) {
+    // }
 }
 
 module.exports = { startNovel };
--- index.js
+++ index.js
@@ -1,23 +1,61 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, StringSelectMenuOptionBuilder, StringSelectMenuBuilder } = require('discord.js');
 const { token } = require('./config.json');
 const { startNovel } = require('./functions/vn');
 
 const client = new Client({
-	intents: [GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMembers,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildPresences,
-	],
+    intents: [GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences,
+    ],
 });
 
 client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-    const { commandName } = interaction;
+    if (interaction.isChatInputCommand()) {
+        const { commandName } = interaction;
 
-    if (commandName === 'startvn') {
-        await interaction.deferReply();
-        await startNovel(interaction);
+        if (commandName === 'startvn') {
+            const embed = new EmbedBuilder()
+                .setColor('Blurple')
+                .setTitle('Welcome!')
+                .setDescription('Thank you for trying our visual novel engine for the first **DDevs Buildathon**!\n\nThis is a story between Hiroshi and Aoi meeting at a local coffee shop. You can start the story by choosing the chapter below.')
+                .setImage('https://res.cloudinary.com/dnjaazvr7/image/upload/v1698906492/discord-buildathon/discord-logo_yeyfyx.png');
+
+            const select = new ActionRowBuilder().setComponents(
+                new StringSelectMenuBuilder()
+                    .setCustomId('chapter')
+                    .setPlaceholder('Choose a chapter.')
+                    .addOptions(
+                        new StringSelectMenuOptionBuilder()
+                            .setLabel('DDevs Demo')
+                            .setValue('demo'),
+                        new StringSelectMenuOptionBuilder()
+                            .setLabel('Chapter 1')
+                            .setValue('chapter-01'),
+                        new StringSelectMenuOptionBuilder()
+                            .setLabel('Chapter 2')
+                            .setValue('chapter-02'),
+                        new StringSelectMenuOptionBuilder()
+                            .setLabel('Chapter 3 - FIN')
+                            .setValue('chapter-03'),
+                    )
+            );
+
+            await interaction.reply({
+                embeds: [embed],
+                components: [select],
+            });
+        }
+    }
+
+    if (interaction.isStringSelectMenu()) {
+        const { customId } = interaction;
+
+        if (customId === 'chapter') {
+            await interaction.deferReply();
+            await startNovel(interaction, interaction.values[0]);
+        }
     }
 });
 
@@ -27,7 +65,7 @@ const data = {
 };
 
 client.once(Events.ClientReady, async c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+    console.log(`Ready! Logged in as ${c.user.tag}`);
 
     await client.application.commands.create(data);
 });
